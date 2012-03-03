7085793392312842bb0e141df2ec3bb0415fe618
Jonathan Rouillard <jonathan.rouillard@gmail.com>
2012-03-03 12:19:47 -0500

    Added yet another bunch of items. Warning: The JSON parser crashes on values at or over 10k, due to how Jagex returns those values. I'll fix this shortly.

--- src/roujo/runescape/data/ItemName.java
+++ src/roujo/runescape/data/ItemName.java
@@ -7,14 +7,25 @@ public enum ItemName {
 	Coconut(5976L),
 	
 	// Ores
+	Copper_ore(436L),
+	Tin_ore(438L),
 	Coal(453L),
 	Iron_ore(440L),
 	Mithril_ore(447L),
 	Adamantite_ore(449L),
+	Runite_ore(451L),
+	Silver_ore(442L),
+	Gold_ore(444L),
 	
 	// Bars
+	Bronze_bar(2349L, new Component[]{new Component(Copper_ore, 1), new Component(Tin_ore, 1)}),
+	Iron_bar(2351L, new Component[]{new Component(Iron_ore, 1)}),
 	Steel_bar(2353L, new Component[]{new Component(Coal, 2), new Component(Iron_ore, 1)}),
-	Mithril_bar(2359L, new Component[]{new Component(Coal, 4), new Component(Mithril_ore, 1)})
+	Mithril_bar(2359L, new Component[]{new Component(Coal, 4), new Component(Mithril_ore, 1)}),
+	Adamant_bar(2361L, new Component[]{new Component(Coal, 6), new Component(Adamantite_ore, 1)}),
+	Rune_bar(2363L, new Component[]{new Component(Coal, 8), new Component(Runite_ore, 1)}),
+	Silver_bar(2355L, new Component[]{new Component(Silver_ore, 1)}),
+	Gold_bar(2355L, new Component[]{new Component(Gold_ore, 1)})
 	;
 	
 	private final long id;
