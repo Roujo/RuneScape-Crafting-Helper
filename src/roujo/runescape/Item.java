2ed0b3148815ff6fc2bcdfa0f1e43162ca670830
Jonathan Rouillard <jonathan.rouillard@gmail.com>
2012-03-02 21:12:01 -0500

    Removing useless imports. Oops. =P

--- src/roujo/runescape/Item.java
+++ src/roujo/runescape/Item.java
@@ -1,9 +1,7 @@
 package roujo.runescape;
 
 import java.awt.Image;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
